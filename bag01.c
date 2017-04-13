f2385b7e884bd11a761cba26c6ef965ae9839d61
ZeaLotDarkLord <704956780@qq.com>
2017-04-13 20:10:35 +0800

    背包01打印选取路径 --ZL

--- bag01.c
+++ bag01.c
@@ -24,49 +24,7 @@ int dp[N][N];
 int n, m;
 int w[N], c[N];
 
-//void bag01(){
-//    int t;
-//    scanf("%d", &t);
-//    while (t--){
-//        memset(dp, 0, sizeof(dp));
-//        memset(w, 0, sizeof(w));
-//        memset(c, 0, sizeof(c));
-//        scanf("%d%d", &n, &m);
-//        int i,j;
-//        for (i = 1; i <= n; i++)
-//            scanf("%d", &w[i]);
-//        for (i = 1; i <= n; i++)
-//            scanf("%d", &c[i]);
-//        for (i = 1; i <= n; i++){
-//            for (j = 0; j <=m; j++){
-//                if (j - c[i] >= 0)
-//                    dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - c[i]] + w[i]);
-//                else
-//                    dp[i][j] = dp[i - 1][j];
-//            }
-//        }
-//        for(i=0; i<=n; i++){
-//            for(j=0; j<=m; j++)
-//                printf("%d ", dp[i][j]);
-//            printf("\n");
-//        }
-//        printf("%d\n", dp[n][m]);
-//    }
-//}
-
-/*
-观察dp[i][j] = max{dp[i-1][j], dp[i-1][j-c[i]]+w[i]}
-第i行第j列只与i-1行第j列和i-1行v-c[i]列有关，
-即本行只与上一行有关，所以可以只用一个一维数组保存第i-1行的各列值，到了i行之后，用原数组的相关值
-来推i行的各列值，然后覆盖原数组，以达到节省空间的目的
-状态转移方程变为:   dp[j] = max(dp[j], dp[j-c[i]]+w[i])
-要注意的是：此处的j要从后往前递减，因为dp[j]的值是和dp[j-c[i]]有关，而j-c[i]<j，也就是说
-如果j从左往右递增的话，dp[j]的值由左边的值推出，dp[j+n]的值也会由左边的值推出，而此时左边的值已经
-被dp[j]及其左边的值给覆盖了
-*/
-/*空间复杂度优化*/
 void bag01(){
-    int dp[N];
     int t;
     scanf("%d", &t);
     while (t--){
@@ -79,18 +37,72 @@ void bag01(){
             scanf("%d", &w[i]);
         for (i = 1; i <= n; i++)
             scanf("%d", &c[i]);
-
         for (i = 1; i <= n; i++){
-            for (j = m; j >= c[i]; j--){
-                dp[j] = max(dp[j], dp[j-c[i]]+w[i]);
+            for (j = 0; j <=m; j++){
+                if (j - c[i] >= 0)
+                    dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - c[i]] + w[i]);
+                else
+                    dp[i][j] = dp[i - 1][j];
             }
         }
-        for(i=0; i<=m; i++)
-            printf("%d ", dp[i]);
-        printf("\n");
-        printf("%d\n", dp[m]);
+        printf("dp数组内容为\n");
+        for(i=0; i<=n; i++){
+            for(j=0; j<=m; j++)
+                printf("%d ", dp[i][j]);
+            printf("\n");
+        }
+        printf("所选择的物品为:\n");
+        i = n;
+        j = m;
+        while(i>0 && j>0){
+            if(dp[i][j] == dp[i-1][j-c[i]]+w[i]){//dp数组中隐藏着路径信息
+                printf("%d ", i);
+                j -= c[i];
+            }
+            i--;
+        }
+
+        printf("\n最大价值为%d\n", dp[n][m]);
     }
 }
+
+/*
+观察dp[i][j] = max{dp[i-1][j], dp[i-1][j-c[i]]+w[i]}
+第i行第j列只与i-1行第j列和i-1行v-c[i]列有关，
+即本行只与上一行有关，所以可以只用一个一维数组保存第i-1行的各列值，到了i行之后，用原数组的相关值
+来推i行的各列值，然后覆盖原数组，以达到节省空间的目的
+状态转移方程变为:   dp[j] = max(dp[j], dp[j-c[i]]+w[i])
+要注意的是：此处的j要从后往前递减，因为dp[j]的值是和dp[j-c[i]]有关，而j-c[i]<j，也就是说
+如果j从左往右递增的话，dp[j]的值由左边的值推出，dp[j+n]的值也会由左边的值推出，而此时左边的值已经
+被dp[j]及其左边的值给覆盖了
+*/
+/*空间复杂度优化，将二维数组优化为一维数组*/
+//void bag01(){
+//    int dp[N];
+//    int t;
+//    scanf("%d", &t);
+//    while (t--){
+//        memset(dp, 0, sizeof(dp));
+//        memset(w, 0, sizeof(w));
+//        memset(c, 0, sizeof(c));
+//        scanf("%d%d", &n, &m);
+//        int i,j;
+//        for (i = 1; i <= n; i++)
+//            scanf("%d", &w[i]);
+//        for (i = 1; i <= n; i++)
+//            scanf("%d", &c[i]);
+//
+//        for (i = 1; i <= n; i++){
+//            for (j = m; j >= c[i]; j--){
+//                dp[j] = max(dp[j], dp[j-c[i]]+w[i]);
+//            }
+//        }
+//        for(i=0; i<=m; i++)
+//            printf("%d ", dp[i]);
+//        printf("\n");
+//        printf("%d\n", dp[m]);
+//    }
+//}
 
 
 int main()
@@ -104,4 +116,13 @@ int main()
 6 10
 5 6 5 1 19 7
 2 3 1 4 6 5
+
+
+0  0  0  0  0  0  0  0  0  0  0
+0  0  5  5  5  5  5  5  5  5  5
+0  0  5  6  6  11 11 11 11 11 11
+0  5  5  10 11 11 16 16 16 16 16
+0  5  5  10 11 11 16 16 16 16 17
+0  5  5  10 11 11 19 24 24 29 30
+0  5  5  10 11 11 19 24 24 29 30
 */
