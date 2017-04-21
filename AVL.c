29ac1b81a54a87cf26dd4bc2076c5b557a17effd
ZeaLotDarkLord <704956780@qq.com>
2017-04-21 23:59:59 +0800

    ZL

--- AVL.c
+++ AVL.c
@@ -49,6 +49,16 @@ static Node* RL(Node* root){
     root->right = LL(root->right);
     return RR(root);
 }
+Node* getMinimumNode(Node* root){
+    if(root == NULL || root->left == NULL)
+        return root;
+    return getMinimumNode(root->left);
+}
+Node* getMaximumNode(Node* root){
+    if(root == NULL || root->right == NULL)
+        return root;
+    return getMaximumNode(root->right);
+}
 //插入  相同值无视
 Node* insertNode(Node* root, int data){
     if(root == NULL)
@@ -56,22 +66,74 @@ Node* insertNode(Node* root, int data){
     else if(data < root->data){
         root->left = insertNode(root->left, data);
         if(getHeight(root->left) - getHeight(root->right) == 2){//在左边插入只可能左边更高
-            if(root->left->data > data)
+            if(getHeight(root->left->left) > getHeight(root->left->right))//也可以比较高度，if(root->left->left的高度 ? root->left->right的高度)
                 root = LL(root);
             else
                 root = LR(root);
         }
     }else if(data > root->data){
         root->right = insertNode(root->right, data);
-        if(getHeight(root->right) - getHeight(root->left) == 2){//在右边插入只可能左边更高
-            if(root->right->data > data)
+        if(getHeight(root->right) - getHeight(root->left) == 2){//在右边插入只可能右边更高
+            if(getHeight(root->right->right) > getHeight(root->right->left))
                 root = RR(root);
             else
                 root = RL(root);
         }
     }
-    root = MAX(getHeight(root->left), getHeight(root->right)) + 1;
+    root->height = MAX(getHeight(root->left), getHeight(root->right)) + 1;
     return root;
+}
+//删除结点
+Node* deleteNode(Node* root, int data){
+    Node* t = NULL;
+    if(root==NULL)//空树
+        return root;
+    if(data < root->data){
+        root->left = deleteNode(root->left, data);
+        if(getHeight(root->right) - getHeight(root->left) == 2){//左边删除只可能是右边更高
+            if(getHeight(root->right->right) > getHeight(root->right->left)){
+                root = RR(root);
+            }else{
+                root = RL(root);
+            }
+        }
+    }else if(data > root->data){
+        root->right = deleteNode(root->right, data);
+        if(getHeight(root->left) - getHeight(root->right) == 2){//右边删除只可能是左边更高
+            if(getHeight(root->left->left) > getHeight(root->left->right)){
+                root = LL(root);
+            }else{
+                root = LR(root);
+            }
+        }
+    }else{//如果找到了
+        if(root->left!=NULL && root->right!=NULL){//有左右孩子
+            if(getHeight(root->left) > getHeight(root->right)){//去高度更高的子树找替代结点
+                t = getMaximumNode(root->left);//去左子树找最大的
+                root->data = t->data;
+                root->left = deleteNode(root->left, t->data);
+            }else{
+                t = getMinimumNode(root->right);//去右子树找最小的
+                root->data = t->data;
+                root->right = deleteNode(root->right, t->data);
+            }
+        }else{//只有一个孩子或者没有孩子
+            t = root;
+            root = root->left != NULL ? root->left : root->right;
+            free(t);
+        }
+    }
+    if(root != NULL)
+        root->height = MAX(getHeight(root->left), getHeight(root->right)) + 1;//更新高度
+    return root;
+}
+Node* searchNode(Node* root, int data){
+    if(root == NULL || root->data == data)
+        return root;
+    else if(root->data > data)
+        return searchNode(root->left, data);
+    else
+        return searchNode(root->right, data);
 }
 //Morris Traversal 中序
 void inOrder(Node* root){
@@ -80,13 +142,13 @@ void inOrder(Node* root){
     while(curr!=NULL){
         if(curr->left!=NULL){
             pre = curr->left;
-            while(pre!=NULL && pre!=curr)
+            while(pre->right!=NULL && pre->right!=curr)
                 pre = pre->right;
-            if(pre == NULL){
-                pre = curr;
+            if(pre->right == NULL){
+                pre->right = curr;
                 curr = curr->left;
             }else{//pre == curr
-                pre = NULL;
+                pre->right = NULL;
                 printf("%d ", curr->data);
                 curr = curr->right;
             }
@@ -95,14 +157,46 @@ void inOrder(Node* root){
             curr = curr->right;
         }
     }
-}
-int main(){
-    int i, arr[] = {5, 2, 8, 4, 1, 9, 7, 6, 3};
-    Node* root = NULL;
-    for(i=0; i<9; i++){
-        root = insertNode(root, arr[i]);
-    }
-    inOrder(root);
-    return 0;
+    printf("\n");
 }
 
+
+#include <windows.h>
+int main(){
+    int i;
+    Node* root = NULL;
+    while(1){
+        system("cls");
+        printf("1.插入元素\n");
+        printf("2.删除元素\n");
+        printf("3.求高度\n");
+        printf("4.求最大值\n");
+        printf("5.求最小值\n");
+        inOrder(root);
+        scanf("%d", &i);
+        switch(i){
+        case 1:
+            printf("请输入待插入元素值:");
+            scanf("%d", &i);
+            root = insertNode(root, i);
+            break;
+        case 2:
+            printf("请输入待删除元素值:");
+            scanf("%d", &i);
+            root = deleteNode(root, i);
+            break;
+        case 3:
+            printf("%d\n", getHeight(root));
+            break;
+        case 4:
+            printf("%d\n", getMaximumNode(root)->height);
+            break;
+        case 5:
+            printf("%d\n", getMinimumNode(root)->height);
+            break;
+        default:break;
+        }
+        //getch();
+    }
+    return 0;
+}
--- AVL.h
+++ AVL.h
@@ -13,8 +13,8 @@ typedef struct Node{
 
 int getHeight(Node* root);                          //获取当前结点所代表的树的高度
 Node* createNode(int data, Node* left, Node* right);//创建一个树结点
-int getMaximumNode(Node* root);                     //取得树中值最大的结点
-int getMinimumNode(Node* root);                     //取得树中值最小的结点
+Node* getMaximumNode(Node* root);                     //取得树中值最大的结点
+Node* getMinimumNode(Node* root);                     //取得树中值最小的结点
 Node* insertNode(Node* root, int data);             //向树中插入新的结点
 Node* deleteNode(Node* root, int data);             //从树中删除指定结点
 void inOrder(Node* root);                           //中序遍历
